4dab50d17247a38491b1b094c61f8efff2777298
drystar <chris.drysdale12@gmail.com>
2019-10-22 20:53:03 +0000

    hello_timeout

---
+++ hello_timeout.js
@@ -0,0 +1,12 @@
+const words = ['4311o', 'th3r3', 'w0r1d']
+
+setTimeout (() => {
+  console.log('4311o')
+}, 2000);
+setTimeout (() => {
+  console.log('th3r3')
+}, 3000);
+
+setTimeout (() => {
+  console.log('thw0r1d3r3')
+}, 4000);
